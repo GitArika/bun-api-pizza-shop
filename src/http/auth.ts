1053d64accf1a3597fef30a6a696843c958b7246
Ariel Evangelista <ariel.evangelista@outlook.com>
2025-03-06 21:20:47 -0300

    feat: error handling & nodemailer integration.

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -2,6 +2,7 @@ import { Elysia, t, type Static } from 'elysia'
 import { jwt } from '@elysiajs/jwt'
 
 import { env } from '../env'
+import { UnauthorizedError } from './errors/unauthorized-error'
 
 export const jwtPayload = t.Object({
   sub: t.String(),
@@ -11,6 +12,16 @@ export const jwtPayload = t.Object({
 export type Payload = Static<typeof jwtPayload>
 
 export const auth = new Elysia()
+  .error({
+    UNAUTHORIZED: UnauthorizedError,
+  })
+  .onError(({ error, code, set }) => {
+    switch (code) {
+      case 'UNAUTHORIZED':
+        set.status = 401
+        return { code, message: error.message }
+    }
+  })
   .use(
     jwt({
       secret: env.JWT_SECRET_KEY,
@@ -38,7 +49,7 @@ export const auth = new Elysia()
 
         const payload = await jwt.verify(authCookie)
 
-        if (!payload) throw new Error('Unauthorized')
+        if (!payload) throw new UnauthorizedError()
 
         return {
           userId: payload.sub,
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,28 @@ import { Elysia } from 'elysia'
 
 import { routes } from './routes'
 
-const app = new Elysia().use(routes)
+const app = new Elysia()
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case 'VALIDATION': {
+        set.status = error.status
+
+        return {
+          code,
+          message: 'Validation faield.',
+          error: error.toResponse(),
+        }
+      }
+      default: {
+        set.status = 500
+
+        console.error(error)
+
+        return new Response(null, { status: 500 })
+      }
+    }
+  })
+  .use(routes)
 
 app.get('/health-check', () => {
   return 'API Online 🚀'
